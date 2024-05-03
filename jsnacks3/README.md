51e0d5ce4b08c98257ed7f9f510ce3daf068d754
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-05-03 12:49:18 +0200

    first commit

---
+++ jsnacks3/README.md
@@ -0,0 +1,2 @@
+1 chiedo all utente di inserirmi 10 numeri
+2 monstro la somma dei valori inseriti in console
---
+++ jsnacks3/js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+//chiedo all utente di inserirmi 10 numeri
+const numero1 = prompt("inserisci il valore 1");
+const numero2 = prompt("inserisci il valore 2");
+const numero3 = prompt("inserisci il valore 3");
+const numero4 = prompt("inserisci il valore 4");
+const numero5 = prompt("inserisci il valore 5");
+const numero6 = prompt("inserisci il valore 6");
+const numero7 = prompt("inserisci il valore 7");
+const numero8 = prompt("inserisci il valore 8");
+const numero9 = prompt("inserisci il valore 9");
+const numero10 = prompt("inserisci il valore 10");
+
+//monstro la somma dei valori inseriti in console (non somma mostra tutt valori in riga)
+const somma = numero1 + numero2 + numero3 + numero4 + numero5 + numero6 + numero7 + numero8 + numero9 + numero10; 
+console.log (somma);
