7b17d090241dd37a3f387a1c6a2b069e54cdfe98
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-05-03 12:29:55 +0200

    correzione snack 2

--- jsnacks2/js/main.js
+++ jsnacks2/js/main.js
@@ -1,4 +1,14 @@
 'use strict';
-
+//parole da inserire da parte dell'utente
 const parola1 = prompt("inserisci una parola");
 const parola2 = prompt("inserisci un altra parola");
+
+if (parola1.length < parola2.length) {
+    console.log ('la parola più corta è', parola1, (parola1.length));
+    console.log ('la parola più lunga è', parola2, (parola2.length))
+} else if (parola2.length < parola1.length) {
+    console.log ('la parola più corta è', parola2, (parola2.length));
+    console.log ('la parola più lunga è', parola1, (parola1.length))
+} else {
+    console.log ('le parole hanno la stessa lunghezza')
+}
